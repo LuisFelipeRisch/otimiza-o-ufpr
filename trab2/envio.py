e156ed72091346b8e63702c1c300fb6041780942
Luis Felipe Risch <luis.risch@visagio.com>
2023-02-17 00:37:04 -0300

    fix: created a true bb

--- trab2/envio.py
+++ trab2/envio.py
@@ -1,80 +1,128 @@
 from argparse import ArgumentParser
-import sys
-import re
-
-restrictions = []
-current_sol = None
-items_weight = None
-problem_params = None
-
 from typing import List, Tuple
+import sys
+import math
+import re
+import copy
 
-def bin_packing(n: int, items: List[Tuple[int, int]], C: int, constraints: List[Tuple[int, int]]) -> List[List[int]]:
-    def bound(level: int, trips: List[List[int]]) -> int:
-        # calculate the remaining weight that can be packed in trips
-        remaining = C
-        for trip in trips:
-            remaining -= sum([items[item][1] for item in trip])
-        # add items to trips to get the maximum number of trips required
-        level = len(trips[-1]) if trips else 0
-        while level < n and items[level][1] <= remaining:
-            remaining -= items[level][1]
-            level += 1
-        return len(trips) + (n - level + len(trips) - 1) // len(trips) + 1
-    
-    def backtrack(level: int, trips: List[List[int]]) -> bool:
-        weight = 0
-        if not trips:
-            weight = 0
+best_trips_qnt = math.inf
+best_trips_arrangement = None
+
+
+def bin_packing(weights:List[int], MAX_CAPACITY:int, constrains: List[Tuple[int, int]]) -> None:
+    global best_trips_arrangement, best_trips_qnt
+
+    def branch_and_bound(start:bool, available_items:List[int], trips:List[List[int]]) -> None:
+        global best_trips_arrangement, best_trips_qnt
+        if start:
+            for item in available_items:
+                branch_and_bound(False, [x for x in available_items if x != item], [[item]])
         else:
-            for item in trips[-1]: 
-                for item_index, item_weight in items:
-                    if item_index == item: 
-                        weight += item_weight
-        if weight > C:
-            # if weight exceeds the capacity, return False
-            return False
-        if level == n:
-            # if all items have been packed, return True
-            return True
-        item = items[level][0]
-        # try packing the current item in each trip
-        for trip in trips:
-            if item not in trip:
-                # check if the constraints are satisfied
-                constraints_satisfied = True
-                for (a, b) in constraints:
-                    if (a in trip and b == item) or (b in trip and a == item):
-                        constraints_satisfied = False
-                        break
-                if constraints_satisfied:
-                   
-                    trip.append(item)
-                    if backtrack(level + 1, trips):
-                        return True
-                    trip.pop()
-        # create a new trip if the current item cannot be packed in any existing trip
-        trips.append([item])
-        if backtrack(level + 1, trips):
-            return True
-        trips.pop()
-        # use branch and bound to prune the search space
-        if bound(level + 1, trips) >= len(trips):
-            return False
-        return backtrack(level + 1, trips)
-    
-    items = sorted(items, key=lambda x: x[1], reverse=True)
-    trips = []
-    backtrack(0, trips)
-    return trips
+            if len(available_items) == 0:
+                if len(trips) < best_trips_qnt:
+                    best_trips_qnt = len(trips)
+                    best_trips_arrangement = copy.deepcopy(trips)
+            else:
+                for current_item in available_items:
+                    found = False
+                    trips_copied = copy.deepcopy(trips)
+
+                    for trip in trips_copied:
+                        if current_item not in trip:
+                            constraints_satisfied = True
+                            for (a, b) in constrains:
+                                if (a in trip and b == current_item) or (b in trip and a == current_item):
+                                    constraints_satisfied = False
+                                    break
+
+                            current_weight = 0
+                            for item in trip:
+                                current_weight += weights[item]
+                            current_weight += weights[current_item]
+
+                            if constraints_satisfied and current_weight <= MAX_CAPACITY:
+                                trip.append(current_item)
+                                found = True
+                                break
+
+                    if not found:
+                        trips_copied.append([current_item])
+
+                    branch_and_bound(False, [x for x in available_items if x != current_item], trips_copied)
+
+    weights = sorted(weights, reverse=True)
+    branch_and_bound(True, [*range(len(weights))], [[]])
+    print(best_trips_qnt)
+    print(best_trips_arrangement)
 
 
-argument_parser = ArgumentParser() 
+# def bin_packing(n: int, items: List[Tuple[int, int]], C: int, constraints: List[Tuple[int, int]]) -> List[List[int]]:
+#     def bound(level: int, trips: List[List[int]]) -> int:
+#         # calculate the remaining weight that can be packed in trips
+#         remaining = C
+#         for trip in trips:
+#             remaining -= sum([items[item][1] for item in trip])
+#         # add items to trips to get the maximum number of trips required
+#         level = len(trips[-1]) if trips else 0
+#         while level < n and items[level][1] <= remaining:
+#             remaining -= items[level][1]
+#             level += 1
+#         return len(trips) + (n - level + len(trips) - 1) // len(trips) + 1
+
+#     def backtrack(level: int, trips: List[List[int]]) -> bool:
+#         for trip in trips:
+#             weight = 0
+#             for item in trip:
+#                 for item_index, item_weight in items:
+#                     if item_index == item:
+#                         weight += item_weight
+#             if weight > C:
+#                 return False
+
+#         if level == n:
+#             # if all items have been packed, return True
+#             return True
+#         item = items[level][0]
+#         # try packing the current item in each trip
+#         for trip in trips:
+#             if item not in trip:
+#                 # check if the constraints are satisfied
+#                 constraints_satisfied = True
+#                 for (a, b) in constraints:
+#                     if (a in trip and b == item) or (b in trip and a == item):
+#                         constraints_satisfied = False
+#                         break
+#                 if constraints_satisfied:
+#                     trip.append(item)
+#                     if backtrack(level + 1, trips):
+#                         return True
+#                     trip.pop()
+#         # create a new trip if the current item cannot be packed in any existing trip
+#         trips.append([item])
+#         if backtrack(level + 1, trips):
+#             return True
+
+#     items = sorted(items, key=lambda x: x[1], reverse=True)
+#     trips = []
+
+#     print(problem_params)
+#     print(items)
+#     print(constraints)
+
+#     backtrack(0, trips)
+#     return trips
+
+
+argument_parser = ArgumentParser()
 argument_parser.add_argument("-f", help="With the -f option on the command line it must turn off the feasibility cuts", action="store_true", default=False)
 argument_parser.add_argument("-o", help="With the -f option on the command line it must turn off the optimally cuts", action="store_true", default=False)
 argument_parser.add_argument("-a", help="with the -a option on the command line it must use the given limiting function by teacher", action="store_true", default=False)
 
 args = argument_parser.parse_args()
+
+restrictions = []
+items_weight = None
+problem_params = None
 
 for line in sys.stdin:
     line = line.strip()
@@ -93,9 +141,6 @@ print(problem_params)
 print(items_weight)
 print(restrictions)
 
-print()
-print([tuple(x) for x in restrictions])
-
-print(bin_packing(len(items_weight), list(zip(range(0, len(items_weight)), items_weight)), problem_params[2], [tuple(x) for x in restrictions]))
+bin_packing(items_weight, problem_params[2], [tuple(x) for x in restrictions])
 
 # print(current_sol)
